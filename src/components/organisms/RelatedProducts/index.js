db83901cc9c19f3d2c530f21c1bd9c30cebba1fb
Adam Mould <adam@adam-mould.co.uk>
2021-10-14 12:08:35 +0100

    Related products only show if there are product

--- src/components/organisms/RelatedProducts/index.js
+++ src/components/organisms/RelatedProducts/index.js
@@ -3,6 +3,8 @@ import ProductCarousel from "components/organisms/ProductCarousel";
 import styles from "./RelatedProducts.module.scss";
 
 const RelatedProducts = ({ title, products }) => {
+  if (!products.length) return null;
+
   return (
     <div className={styles.wrap}>
       <h3 className={styles.title}>{title}</h3>
