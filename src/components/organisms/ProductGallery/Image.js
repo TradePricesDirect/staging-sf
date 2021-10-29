8b7ca10102eafdfe7e9ea8f7daa11fc4d8e8fbf9
Adam Mould <adam@adam-mould.co.uk>
2021-10-29 09:23:00 +0100

    Fixed image sizing

--- src/components/organisms/ProductGallery/Image.js
+++ src/components/organisms/ProductGallery/Image.js
@@ -5,12 +5,15 @@ import styles from "./ProductGallery.module.scss";
 const Image = ({ image }) => {
   return (
     <div className={styles.slide}>
-      <SideBySideMagnifier
-        imageSrc={image.url}
-        alt={image.alt}
-        alwaysInPlace
-        transitionSpeedInPlace={0.2}
-      />
+      <div className={styles.slideInner}>
+        <SideBySideMagnifier
+          className={styles.slideImage}
+          imageSrc={image.url}
+          alt={image.alt}
+          alwaysInPlace
+          transitionSpeedInPlace={0.2}
+        />
+      </div>
     </div>
   );
 };
