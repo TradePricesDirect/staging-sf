4243141cb6eeb2111d82d3ba7029914f2dda7f9f
Adam Mould <adam@adam-mould.co.uk>
2021-11-03 15:12:54 +0000

    Modal overflow

--- src/components/molecules/OrderTable/index.js
+++ src/components/molecules/OrderTable/index.js
@@ -18,38 +18,44 @@ const OrderTable = ({ orders }) => {
   }
 
   return (
-    <table className="table table-borderless table-striped align-middle">
-      <thead>
-        <tr>
-          <th scope="col">Order No.</th>
-          <th scope="col">Order Placed</th>
-          <th scope="col">Total</th>
-          <th scope="col">Status</th>
-          <th scope="col" className={styles.actionColumn}></th>
-        </tr>
-      </thead>
-      <tbody>
-        {orders.map(({ id, number, created, total, statusDisplay, token }) => (
-          <tr key={id}>
-            <td scope="row">{number}</td>
-            <td>{formatDate(created)}</td>
-            <td>
-              <Money money={total.gross} />
-            </td>
-            <td>
-              <OrderStatus status={statusDisplay} />
-            </td>
-            <td className={styles.actionColumn}>
-              <Link href={{ pathname: paths.account.order, query: { token } }}>
-                <a className="btn btn-sm btn-outline-primary">
-                  <FontAwesomeIcon icon={faEye} />
-                </a>
-              </Link>
-            </td>
+    <div className="table-responsive">
+      <table className="table table-borderless table-striped align-middle">
+        <thead>
+          <tr>
+            <th scope="col">Order No.</th>
+            <th scope="col">Order Placed</th>
+            <th scope="col">Total</th>
+            <th scope="col">Status</th>
+            <th scope="col" className={styles.actionColumn}></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {orders.map(
+            ({ id, number, created, total, statusDisplay, token }) => (
+              <tr key={id}>
+                <td scope="row">{number}</td>
+                <td>{formatDate(created)}</td>
+                <td>
+                  <Money money={total.gross} />
+                </td>
+                <td>
+                  <OrderStatus status={statusDisplay} />
+                </td>
+                <td className={styles.actionColumn}>
+                  <Link
+                    href={{ pathname: paths.account.order, query: { token } }}
+                  >
+                    <a className="btn btn-sm btn-outline-primary">
+                      <FontAwesomeIcon icon={faEye} />
+                    </a>
+                  </Link>
+                </td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
