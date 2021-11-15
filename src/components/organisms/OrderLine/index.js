2111f84eb5a7c34cd6f13d6c7eaa83114bc8c62c
Adam Mould <adam@adam-mould.co.uk>
2021-11-15 20:25:05 +0000

    Orderline if product has been deleted

--- src/components/organisms/OrderLine/index.js
+++ src/components/organisms/OrderLine/index.js
@@ -5,32 +5,49 @@ import TaxedMoney from "components/molecules/TaxedMoney";
 
 import styles from "./OrderLine.module.scss";
 
-const OrderLine = ({ variant, quantity, unitPrice, totalPrice }) => {
+const OrderLine = ({
+  variant,
+  quantity,
+  unitPrice,
+  totalPrice,
+  productName,
+}) => {
   return (
     <tr>
       <td scope="row">
         <div className={styles.content}>
-          <Link href={paths.product.replace("[slug]", variant.product.slug)}>
-            <a className={styles.thumbnail}>
-              <Thumbnail thumbnail={variant.product.thumbnail} />
-            </a>
-          </Link>
+          {variant && (
+            <Link href={paths.product.replace("[slug]", variant.product.slug)}>
+              <a className={styles.thumbnail}>
+                <Thumbnail thumbnail={variant.product.thumbnail} />
+              </a>
+            </Link>
+          )}
 
           <div className={styles.body}>
-            <Link href={paths.product.replace("[slug]", variant.product.slug)}>
-              <a className={styles.name}>{variant.product.name}</a>
-            </Link>
+            {variant ? (
+              <>
+                <Link
+                  href={paths.product.replace("[slug]", variant.product.slug)}
+                >
+                  <a className={styles.name}>{variant.product.name}</a>
+                </Link>
 
-            <p className={styles.sku}>{variant.sku}</p>
-
-            <ul className={styles.variants}>
-              {variant.attributes.map(({ attribute, values }) => (
-                <li key={attribute.id} className={styles.variant}>
-                  {attribute.name}:{" "}
-                  <span>{values.map((value) => value.name).join(", ")}</span>
-                </li>
-              ))}
-            </ul>
+                <p className={styles.sku}>{variant.sku}</p>
+                <ul className={styles.variants}>
+                  {variant.attributes.map(({ attribute, values }) => (
+                    <li key={attribute.id} className={styles.variant}>
+                      {attribute.name}:{" "}
+                      <span>
+                        {values.map((value) => value.name).join(", ")}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </>
+            ) : (
+              <div className={styles.name}>{productName}</div>
+            )}
           </div>
         </div>
       </td>
