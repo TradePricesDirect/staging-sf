0e832a36d81dc30a78638b4982660d3c06e3b7bc
Adam Mould <adam@adam-mould.co.uk>
2021-11-30 20:31:12 +0000

    Bugsnag prevent multiple loads

--- src/utils/bugsnag.js
+++ src/utils/bugsnag.js
@@ -1,9 +1,11 @@
 import Bugsnag from "@bugsnag/js";
 import BugsnagPluginReact from "@bugsnag/plugin-react";
 
-Bugsnag.start({
-  apiKey: process.env.NEXT_PUBLIC_BUGSNAG_API_KEY,
-  plugins: [new BugsnagPluginReact()],
-});
+if (!Bugsnag._client) {
+  Bugsnag.start({
+    apiKey: process.env.NEXT_PUBLIC_BUGSNAG_API_KEY,
+    plugins: [new BugsnagPluginReact()],
+  });
+}
 
 export default Bugsnag;
